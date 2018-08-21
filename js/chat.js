39b7651dabbec290edf8884b43509fcae2f67c62
Joel Serino <joel@thinq4yourself.com>
2018-08-20 17:03:18 -0700

    Adding native facebook messenger

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,4 @@
-/*(function () {
+(function () {
     var options = {
         facebook: "130136700527470", // Facebook page ID
         call_to_action: "Message me", // Call to action
@@ -9,11 +9,3 @@
     s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
     var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
 })();
-*/
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
