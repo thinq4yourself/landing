1990b9d905c13af756fae35194ab2546f28b3805
Joel Serino <joel@thinq4yourself.com>
2018-08-20 17:12:58 -0700

    trying fb messenger activation via click events

--- js/chat.js
+++ js/chat.js
@@ -1,11 +1,3 @@
-(function () {
-    var options = {
-        facebook: "130136700527470", // Facebook page ID
-        call_to_action: "Message me", // Call to action
-        position: "right", // Position may be 'right' or 'left'
-    };
-    var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-})();
+$('.messenger-click').on('click', function(){
+    FB.CustomerChat.show(shouldShowDialog: boolean);
+})
