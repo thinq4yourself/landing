84d0c0f5e02b1689f6f02d40a0a434718e4efd33
Joel Serino <joel@thinq4yourself.com>
2018-08-28 21:25:32 -0700

    updated FB chat and payment for testing...again

--- js/fb.js
+++ js/fb.js
@@ -2,19 +2,10 @@ window.fbAsyncInit = function() {
     FB.init({
       appId            : '219911925347660',
       autoLogAppEvents : true,
-      xfbml            : true,
+      xfbml            : false,
       version          : 'v3.1'
     });
 };
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 
 $('.open-messenger').on('click', function(){
     FB.CustomerChat.showDialog();
