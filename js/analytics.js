ff148f2c18e20bc41e58262dc1f3081a63a4399b
Joel Serino <joel@thinq4yourself.com>
2018-09-20 14:52:47 -0700

    Added lightarian payment link to training page. Updated goden dna timer Updated Avatar timer

--- js/analytics.js
+++ js/analytics.js
@@ -2,3 +2,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-42099274-1');
+
+$( '#$addToCartButton' ).click(function() {
+  fbq('track', 'AddToCart');
+}
